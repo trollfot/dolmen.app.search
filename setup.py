3b3b1468c6cee482d7793290877443d28b299b9c
Vincent Fretin <vincent.fretin@gmail.com>
2010-07-24 17:31:51 +0200

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.app.search'
-version = '0.3'
+version = '0.4dev'
 readme = open(join('src', 'dolmen', 'app', 'search', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
